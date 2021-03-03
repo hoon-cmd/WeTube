d63bd6025de1ce2733fd9f4e10843b43814af88e
hoon-cmd <77324409+hoon-cmd@users.noreply.github.com>
2021-03-03 10:36:00 +0900

    node.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Hi, I'm node.JS");
+import "core-js";
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => console.log(`Listening on: localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Hello World");
+
+const handleProfile = (req, res) => res.send("This is your profile page");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(helmet());
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
